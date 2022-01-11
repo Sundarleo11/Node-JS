17a5a08034d3c132d40d9bb7ad5772c7c5f85fa7
Sundarleo11 <sundarrock1999@gmail.comm>
2022-01-11 20:57:56 +0530

    intro
    Signed-off-by: Sundarleo11 <sundarrock1999@gmail.comm>

---
+++ Nodejs_Intro/lib.js
@@ -0,0 +1,22 @@
+//window function using browser
+
+/*
+
+window.aws=function(){
+    console.log("This a sample source code");
+}
+
+*/
+
+function sample() {
+    console.log('Node sharing source code');
+    
+}
+
+function awesomefunction() {
+    console.log('This a awesomefunction');
+    
+}
+module.export={
+    awesomefunction
+}
---
+++ Nodejs_Intro/script.js
@@ -0,0 +1,8 @@
+//aws();
+
+const libes= require('./lib.js').default;
+
+
+console.log(libes);
+//lib.sample();
+
